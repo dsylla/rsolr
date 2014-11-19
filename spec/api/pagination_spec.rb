898b2b34185df64a36bb63adbbb1c0ee9d583a4b
David Sylla <d.sylla@xair.fr>
2014-11-19 20:50:48 +0100

    fix paginate params + specs

--- spec/api/pagination_spec.rb
+++ spec/api/pagination_spec.rb
@@ -6,8 +6,8 @@ describe "RSolr::Pagination" do
       r = c.build_paginated_request 3, 25, "select", {:params => {:q => "test"}}
       #r[:page].should == 3
       #r[:per_page].should == 25
-      expect(r[:params]["start"]).to eq(50)
-      expect(r[:params]["rows"]).to eq(25)
+      expect(r[:params][:start]).to eq(50)
+      expect(r[:params][:rows]).to eq(25)
       expect(r[:uri].query).to match(/rows=25/)
       expect(r[:uri].query).to match(/start=50/)
     end
@@ -19,8 +19,8 @@ describe "RSolr::Pagination" do
         #:page => 1,
         #:per_page => 10,
         :params => {
-          "rows" => 10,
-          "start" => 0,
+          :rows => 10,
+          :start => 0,
           :wt => :ruby
         }
       }))
